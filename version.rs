f636f8d66eb2e2f2019b8b0a0467a77b2fd133a2
clux <sszynrae@gmail.com>
2019-12-13 04:54:43 +0000

    make async version work with caveats
    - pinned master versions of reqwest + kube
    - no actix prometheus transformer available yet
    - reflector idea now a bit dumb here with 300s wait
    
    will concentrate on the third problem here.
    kube can probably expose something nicer for apps to deal with this
    rather than having to maintain the full informer..

--- version.rs
+++ version.rs
@@ -1,9 +1,11 @@
 #[macro_use] extern crate log;
 #[macro_use] extern crate serde_derive;
 use actix_web::{
-  web::{self, Data},
-  App, HttpServer, HttpRequest, HttpResponse, middleware,
+    web::{Data},
+    HttpRequest, HttpResponse, middleware
 };
+use actix_web::{get, App, HttpServer, Responder};
+
 use kube::{
     client::APIClient,
     config::Configuration,
@@ -12,7 +14,7 @@ use kube::{
 use k8s_openapi::api::apps::v1::{DeploymentSpec, DeploymentStatus};
 use std::sync::{Arc, RwLock};
 
-type Result<T> = std::result::Result<T, failure::Error>;
+type Result<T> = std::result::Result<T, anyhow::Error>;
 type Deploy = Object<DeploymentSpec, DeploymentStatus>;
 
 #[derive(Serialize, Clone)]
@@ -45,32 +47,34 @@ pub struct Watcher {
 }
 
 impl Watcher {
-    pub fn init(cfg: Configuration) -> Result<Watcher> {
-        let c = Watcher::new(APIClient::new(cfg))?; // for app to read
-        let c2 = c.clone(); // for poll thread to write
-        std::thread::spawn(move || {
+    pub async fn init(cfg: Configuration) -> Result<Watcher> {
+        let c = Watcher::new(APIClient::new(cfg)).await?; // for app to read
+        let c2 = c.clone(); // for looping task below
+        tokio::spawn(async move {
             loop {
-                let _ = c2.poll().map_err(|e| {
+                if let Err(e) = c2.poll().await {
                     error!("Kube state failed to recover: {}", e);
                     std::process::exit(1); // kube will restart here
-                });
+                }
             }
         });
         Ok(c)
     }
-    fn new(client: APIClient) -> Result<Self> {
+    async fn new(client: APIClient) -> Result<Self> {
         let namespace = std::env::var("NAMESPACE").unwrap_or("default".into());
         let resource = Api::v1Deployment(client.clone()).within(&namespace);
-        let rf = Reflector::new(resource).init()?;
-        let state = Arc::new(RwLock::new(Vec::new()));
+        let rf = Reflector::new(resource).init().await?;
+        let state = Arc::new(RwLock::new(Watcher::read(&rf).await?));
         Ok(Watcher { rf, state, client })
     }
-    fn poll(&self) -> Result<()> {
-        self.rf.poll()?;
-        let state = self.rf.read()?.into_iter()
-            .filter_map(|(_, d)| Entry::from(d))
-            .collect();
-        *self.state.write().unwrap() = state;
+    async fn read(rf: &Reflector<Deploy>) -> Result<Vec<Entry>> {
+        Ok(rf.state().await?.into_iter()
+            .filter_map(Entry::from)
+            .collect())
+    }
+    async fn poll(&self) -> Result<()> {
+        self.rf.poll().await?; // update cache every re-sync
+        *self.state.write().unwrap() = Watcher::read(&self.rf).await?;
         Ok(())
     }
     pub fn state(&self) -> Vec<Entry> {
@@ -78,33 +82,36 @@ impl Watcher {
     }
 }
 
-fn versions(c: Data<Watcher>, _req: HttpRequest) -> HttpResponse {
+#[get("/")]
+async fn versions(c: Data<Watcher>, _req: HttpRequest) -> impl Responder {
     HttpResponse::Ok().json(c.state())
 }
-fn health(_: HttpRequest) -> HttpResponse {
+#[get("/health")]
+async fn health(_: HttpRequest) -> impl Responder {
     HttpResponse::Ok().json("healthy")
 }
 
-fn main() {
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
     env_logger::init();
-    let cfg = kube::config::incluster_config().or_else(|_| {
-        kube::config::load_kube_config()
-    }).expect("Failed to load kube config");
-    let c = Watcher::init(cfg).expect("Failed to initialize watcher");
+    let cfg = if let Ok(c) = kube::config::incluster_config() {
+        c
+    } else {
+        kube::config::load_kube_config().await.expect("Failed to load kube config")
+    };
+    let c = Watcher::init(cfg).await.expect("Failed to initialize watcher");
 
-    let sys = actix::System::new("version");
-    let prometheus = actix_web_prom::PrometheusMetrics::new("api", "/metrics");
+    //let prometheus = actix_web_prom::PrometheusMetrics::new("api", "/metrics");
     HttpServer::new(move || {
         App::new()
             .data(c.clone())
             .wrap(middleware::Logger::default().exclude("/health"))
-            .wrap(prometheus.clone())
-            .service(web::resource("/").to(versions))
-            .service(web::resource("/health").to(health))
+            //.wrap(prometheus.clone())
+            .service(versions)
+            .service(health)
         })
         .bind("0.0.0.0:8000").expect("Can not bind to 0.0.0.0:8000")
         .shutdown_timeout(0)
-        .start();
-    info!("Starting listening on 0.0.0.0:8000");
-    let _ = sys.run();
+        .start()
+        .await
 }
